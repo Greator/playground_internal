0bf45f1b75424116df822952b5f5ea7851f32e1c
FKasa <flaflamuri@gmail.com>
2021-08-24 16:34:04 +0200

    added label to settings and new setting for quiet hours

--- web/app/plugins/playground-plugin/playground-plugin.php
+++ web/app/plugins/playground-plugin/playground-plugin.php
@@ -17,9 +17,11 @@ class Playground {
   function settings(){
     add_settings_section('section1', null, null, 'playground-settings-page');
 
-    add_settings_field('playground_sidebar_show_searchbox', 'Display searchbox on sidebar', array($this, 'showSidebarSearchHTML'), 'playground-settings-page', 'section1');
+    add_settings_field('playground_sidebar_show_searchbox', 'Sidebar', array($this, 'showSidebarSearchHTML'), 'playground-settings-page', 'section1');
     register_setting('playgroungplugin','playground_sidebar_show_searchbox',array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
 
+    add_settings_field('playground_quiet_hours', 'Quite hours', array($this, 'quietHoursHTML'), 'playground-settings-page', 'section1');
+    register_setting('playgroungplugin','playground_quiet_hours',array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
   }
 
   function adminPage(){
@@ -27,7 +29,13 @@ class Playground {
   }
 
   function showSidebarSearchHTML(){ ?>
-    <input type="checkbox" name="playground_sidebar_show_searchbox" value="1" <?php checked(get_option('playground_sidebar_show_searchbox', '0')); ?> >
+    <input type="checkbox" name="playground_sidebar_show_searchbox" value="1" <?php checked(get_option('playground_sidebar_show_searchbox', '0')); ?>  id="playground_sidebar_show_searchbox">
+    <label for="playground_sidebar_show_searchbox">Display searchbox in sidebar</label>
+  <?php }
+
+  function quietHoursHTML(){ ?>
+    <input type="checkbox" name="playground_quiet_hours" value="1" <?php checked(get_option('playground_quiet_hours', '1')); ?> id="playground_quiet_hours">
+    <label for="playground_quiet_hours">Show 0 notifications from 8pm to 7am</label>
   <?php }
 
   function optionsHMTL(){ ?>
